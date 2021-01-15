389c99e3c5a286894148d5867b3958bdd35d8384
李龙 <1306205691@qq.com>
2021-01-15 16:54:45 +0800

    feat(all): 新增nacos服务注册和配置中心建联

---
+++ manager/src/main/java/com/demons/manager/ManagerApplication.java
@@ -0,0 +1,16 @@
+package com.demons.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class ManagerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ManagerApplication.class, args);
+  }
+
+}
