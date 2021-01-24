9cdf4f39d19350329f436708db2eaec9f9f9ce37
李龙 <1306205691@qq.com>
2021-01-24 12:37:29 +0800

    feat(all): 调试gateway模块

--- gateway/src/main/java/com/manager/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/manager/gateway/GatewayApplication.java
@@ -3,11 +3,12 @@ package com.manager.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Outro
+ */
 @SpringBootApplication
 public class GatewayApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(GatewayApplication.class, args);
   }
-
 }
